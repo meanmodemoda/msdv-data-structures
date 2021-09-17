6177d72200cf04da34e38d0ceab913ed8513816b
EC2 Default User <ec2-user@ip-172-31-30-0.ec2.internal>
2021-09-17 03:07:30 +0000

    re-run

--- starter-files/starter-code-week03.js
+++ starter-files/starter-code-week03.js
@@ -1,26 +1,50 @@
+"use strict"
 
-var fs = require('fs');
-var cheerio = require('cheerio');
+// dependencies
+const fs = require('fs'),
+      querystring = require('querystring'),
+      got = require('got'),
+      async = require('async'),
+      dotenv = require('dotenv');
 
-// load the thesis text file into a variable, `content`
-// this is the file that we created in the starter code from last week
-var content = fs.readFileSync('data/thesis.txt');
+// TAMU api key
+dotenv.config();
+const API_KEY = process.env.TAMU_KEY;
+const API_URL = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx'
 
-// load `content` into a cheerio object
-var $ = cheerio.load(content);
+// geocode addresses
+let meetingsData = [];
+let addresses = ["63 Fifth Ave", "16 E 16th St", "2 W 13th St"];
 
-// print (to the console) names of thesis students
-$('h3').each(function(i, elem) {
-    console.log($(elem).text());
+// eachSeries in the async module iterates over an array and operates on each item in the array in series
+async.eachSeries(addresses, function(value, callback) {
+    let query = {
+        streetAddress: value,
+        city: "New York",
+        state: "NY",
+        apikey: API_KEY,
+        format: "json",
+        version: "4.01"
+    };
+
+    // construct a querystring from the `query` object's values and append it to the api URL
+    let apiRequest = API_URL + '?' + querystring.stringify(query);
+
+    (async () => {
+    	try {
+    		const response = await got(apiRequest);
+    		let tamuGeo = JSON.parse(response.body);
+    		console.log(tamuGeo['FeatureMatchingResultType'], apiRequest);
+    		meetingsData.push(tamuGeo);
+    	} catch (error) {
+    		console.log(error.response.body);
+    	}
+    })();
+
+    // sleep for a couple seconds before making the next request
+    setTimeout(callback, 2000);
+}, function() {
+    fs.writeFileSync('data/first.json', JSON.stringify(meetingsData));
+    console.log('*** *** *** *** ***');
+    console.log(`Number of meetings in this zone: ${meetingsData.length}`);
 });
-
-// write the project titles to a text file
-var thesisTitles = ''; // this variable will hold the lines of text
-
-$('.project .title').each(function(i, elem) {
-    thesisTitles += ($(elem).text()).trim() + '\n';
-});
-
-fs.writeFileSync('data/thesisTitles.txt', thesisTitles);
-
-
